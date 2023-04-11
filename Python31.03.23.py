99a67f066625c853d9fd51894fd12bc122a60063
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 11:43:26 +0300

    tusk dune

--- Python31.03.23.py
+++ Python31.03.23.py
@@ -1,1 +1,12 @@
 
+numbers = input("Enter numbers separated by a space: ").split()
+numbers = [int(num) for num in numbers]
+
+arithmetic_mean = sum(numbers) / len(numbers)
+
+count = 0
+for num in numbers:
+    if num > average:
+        count += 1
+
+print("The number of numbers greater than the arithmetic mean:", count)
