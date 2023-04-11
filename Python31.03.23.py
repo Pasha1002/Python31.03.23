6bd8654aab9843fd0ef953a2e1f8705ddc70da0b
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 11:11:03 +0300

    tusk dune

--- Python31.03.23.py
+++ Python31.03.23.py
@@ -1,1 +1,10 @@
 
+numbers = input("Enter numbers with different signs separated by a space: ").split()
+numbers = [int(num) for num in numbers]
+
+positive_numbers = []
+for num in numbers:
+    if num > 0:
+        positive_numbers.append(num)
+
+print("Positive numbers that were in the list: ", positive_numbers)
