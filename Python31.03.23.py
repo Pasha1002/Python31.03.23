e326a648d1ddf4828eff9960436c2b568fa4d13e
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 10:49:29 +0300

    tusk dune

--- Python31.03.23.py
+++ Python31.03.23.py
@@ -1,1 +1,17 @@
 
+import random
+list = []
+for i in range(1,10):
+    list.append(random.randint(-10,10))
+print(list)
+#1
+print('\n')
+even_numbers = 0
+odd_numbers = 0 
+for i in list:
+    if i %2 == 0:
+        even_numbers += 1
+    else:
+        odd_numbers += 1
+print('Number of even numbers:',even_numbers)
+print('Number of odd numbers:',odd_numbers)
