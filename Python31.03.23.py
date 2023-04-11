e92110d9085f4fc7f3720bbfbd12d0b3cbf2b593
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 11:32:25 +0300

    tusk dune

--- Python31.03.23.py
+++ Python31.03.23.py
@@ -1,1 +1,10 @@
 
+numbers = input("Enter the numbers through the space: ").split()
+numbers = [int(num) for num in numbers]
+
+squaeres_of_numbers = []
+for num in numbers:
+    if num > 0:
+        squaeres_of_numbers.append(num**2)
+
+print("Squares of the numbers in the list: ", squaeres_of_numbers)
