7afbe52c6147272fdc43492a76bd8e4f8b4936eb
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 10:35:16 +0300

    tusk dune

--- Python31.03.23.py
+++ Python31.03.23.py
@@ -1,1 +1,26 @@
 
+import random
+list = []
+for i in range(1,10):
+    list.append(random.randint(-10,10))
+print(list)
+#1
+print("\n")
+for i in list:
+    if i %2 ==0:
+        print('Even numbers from the first list:',i)
+#2
+print("\n")
+for i in list:
+    if i %2 !=0:
+        print('Odd numbers from the first list:',i)
+#3
+print("\n")
+for i in list:
+    if i < 0:
+        print('Negative numbers from the first list:',i)
+#4
+print("\n")
+for i in list:
+    if i > 0:
+        print('Positive numbers from the first list:',i)
