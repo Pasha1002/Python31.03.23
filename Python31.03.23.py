9ba765883d1f8f5005a080e87e28949ecb6da16d
Pasha1002 <kanavetspavel2001@gmail.com>
2023-04-11 11:37:43 +0300

    tusk dune

--- Python31.03.23.py
+++ Python31.03.23.py
@@ -1,1 +1,11 @@
 
+numbers = input("Enter numbers separated by a space: ").split()
+numbers = [int(num) for num in numbers]
+
+min_ind = numbers.index(min(numbers))
+max_ind = numbers.index(max(numbers))
+
+numbers[min_ind], numbers[max_ind] = numbers[max_ind], numbers[min_ind]
+
+print("A list with changed places of the minimum and maximum elements: ")
+print(numbers)
